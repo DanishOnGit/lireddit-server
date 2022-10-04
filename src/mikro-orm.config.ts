4eee8593e271eb0a72ff4cfa1f1619e032e3debb
DanishOnGit <youcandoit1911@gmail.com>
2022-10-04 23:00:33 +0530

    postgres,mikroorm setup

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+console.log('dirname',__dirname)
+export default  {
+    migrations:{
+        path:path.join(__dirname,"./migrations"),
+        pattern:/^[\w-]+\d+\.[tj]s$/,
+    },
+    entities:[Post],
+    dbName:"postgres",
+    type:"postgresql",
+    user:"postgres",
+    password:"D@nny123",
+    allowGlobalContext:true,
+    debug:!__prod__,
+} as Parameters <typeof MikroORM.init>[0] 
