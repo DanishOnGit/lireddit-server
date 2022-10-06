ab0def7d03c3be97e12b3ae75f4f7dc1ef1d3056
DanishOnGit <youcandoit1911@gmail.com>
2022-10-06 23:01:43 +0530

    Apollo-gql server setup complete
    server tested with a dummy resolver. Looks good!

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
 
 const main=async()=>{
+  //db connection
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
-  const post=  orm.em.create(Post,{title:'my first post'})
-  await orm.em.persistAndFlush(post)
+
+    //server start
+    const app = express();
+    app.get('/',(_,res)=>{
+      res.status(200).json('Succesfully connected')
+    })
+
+    const apolloServer =  new ApolloServer({
+        schema: await buildSchema({
+          resolvers:[HelloResolver],
+          validate:false
+        })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({
+      app,
+    });
+    
+    app.listen(4000,()=>{
+      console.log('Server started on localhost:4000')
+    })
 }
 main().catch((err)=>console.log(err))
